724094892ba5a6342f79c81603d822d81f9f8f6b
Joseph Villajin <resetdesigns@gmail.com>
2022-04-12 16:35:22 -0700

    created views and partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,20 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
@@ -24,7 +34,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
-        message: 'This is an example message',
+        name: 'Joseph Villajin',
+        helpText: 'This is an example message',
     });
 });
 
@@ -35,8 +46,20 @@ app.get('/weather', (req, res) => {
     });
 });
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Joseph Villajin',
+        errorMessage: 'Help article not found.',
+    });
+});
+
 app.get('*', (req, res) => {
-    res.send('404');
+    res.render('404', {
+        title: '404',
+        name: 'Joseph Villajin',
+        errorMessage: 'Page not found.',
+    });
 });
 
 app.listen(3000, () => {
