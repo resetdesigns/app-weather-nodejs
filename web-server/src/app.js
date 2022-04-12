f93ced2fc51cfc16a7543fa9eb9d2dfa2e17addb
Joseph Villajin <resetdesigns@gmail.com>
2022-04-12 15:53:58 -0700

    switched static files to handlebars templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,29 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Joseph Villajin',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Joseph Villajin',
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'This is an example message',
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
