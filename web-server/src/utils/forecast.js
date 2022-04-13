c6f0681546131f0dcf9729f32869626fa9e96d0c
Joseph Villajin <resetdesigns@gmail.com>
2022-04-12 22:07:16 -0700

    added geocode and forecast apis

---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY_FORECAST}&query=${latitude},${longitude}&units=f`;
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service.', undefined);
+        } else if (body.error) {
+            callback('Unable to find location.', undefined);
+        } else {
+            callback(
+                undefined,
+                `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. There is a ${body.current.precip}% chance of rain`
+            );
+        }
+    });
+};
+
+module.exports = forecast;
