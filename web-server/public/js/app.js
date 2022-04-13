a2b6a57aa096b9272cff6b62206b6d8bba9cf54e
Joseph Villajin <resetdesigns@gmail.com>
2022-04-12 22:43:16 -0700

    Connect Functionality to User Input

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,24 @@
-console.log('Client side JavaScript file');
+const weatherForm = document.querySelector('form#weather-form');
+const search = document.querySelector('form#weather-form input');
+const messageOne = document.querySelector('p#message-1');
+const messageTwo = document.querySelector('p#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        });
+    });
+});
