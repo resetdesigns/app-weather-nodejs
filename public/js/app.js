1ba57ce6ec3e2cd242e9f695529d0f0efbead2ab
Joseph Villajin <resetdesigns@gmail.com>
2022-06-13 15:38:53 -0700

    Added Geolocation Support

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,50 @@
 const weatherForm = document.querySelector('form#weather-form');
 const search = document.querySelector('form#weather-form input');
+const btnGeo = document.querySelector('#btn-geo');
 const messageOne = document.querySelector('p#message-1');
 const messageTwo = document.querySelector('p#message-2');
 
+btnGeo.addEventListener('click', (e) => {
+	e.preventDefault();
+
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition((position) => {
+			const posLat = position.coords.latitude;
+			const posLon = position.coords.longitude;
+
+			messageOne.textContent = 'Loading...';
+			messageTwo.textContent = '';
+
+			fetch('/geo-weather?lat=' + posLat + '&lon=' + posLon).then((response) => {
+				response.json().then((data) => {
+					if (data.error) {
+						messageOne.textContent = data.error;
+					} else {
+						messageOne.textContent = data.location;
+						messageTwo.textContent = data.forecast;
+					}
+				});
+			});
+		});
+	}
+});
+
 weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+	e.preventDefault();
 
-    const location = search.value;
+	const location = search.value;
 
-    messageOne.textContent = 'Loading...';
-    messageTwo.textContent = '';
+	messageOne.textContent = 'Loading...';
+	messageTwo.textContent = '';
 
-    fetch('/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error;
-            } else {
-                messageOne.textContent = data.location;
-                messageTwo.textContent = data.forecast;
-            }
-        });
-    });
+	fetch('/weather?address=' + location).then((response) => {
+		response.json().then((data) => {
+			if (data.error) {
+				messageOne.textContent = data.error;
+			} else {
+				messageOne.textContent = data.location;
+				messageTwo.textContent = data.forecast;
+			}
+		});
+	});
 });
